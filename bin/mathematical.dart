9522e76137e985f47dbcfa35d48753558051f5a8
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2025-04-17 13:26:43 +0700

    operator mathematic

--- bin/mathematical.dart
+++ bin/mathematical.dart
@@ -1,5 +1,27 @@
 void main(List<String> arguments) {
+  // operator aritmatika
   double number;
-  number = 5 + 2;
+  // pembagian ini menghasilkan bilangan berkoma
+  number = 5 / 2;
+  int number2;
+  // pembagian ini menghasilkan bilangan bulat
+  number2 = 5 ~/ 2;
+
+  int number3;
+  number3 = 10;
+  number3 += 10;
+
   print(number);
+  print(number2);
+  print(number3);
+
+  // operator perbandingan
+  bool result;
+  result = 5 < 9;
+  print(result);
+
+  // operator logikal
+  bool result2;
+  result2 = (5 < 9) && (10 > 5);
+  print(result2);
 }
