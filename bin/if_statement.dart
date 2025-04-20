94f0458ed8d1ff20862a4f33473edc27ba6dd9d6
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2025-04-20 23:13:08 +0700

    if statement

---
+++ bin/if_statement.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  String input = stdin.readLineSync() ?? 'no input';
+  int temperature = int.tryParse(input) ?? 0;
+  if (temperature < 0) {
+    print('frozen');
+  } else if (temperature > 0 && temperature <= 100) {
+    print('liquid');
+  } else {
+    print('steam');
+  }
+
+  int number = int.tryParse(input) ?? 0;
+  int result = number > 0 ? number : number * -1;
+  print(result);
+}
